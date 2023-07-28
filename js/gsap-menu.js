c4041faae3a66dd4f946997cb9bcd71d7f48416a
Tomoya Satoh <tomo@TomoyanoMacBook-Pro.local>
2023-07-28 21:02:07 +0900

    html css javascript 完了

--- js/gsap-menu.js
+++ js/gsap-menu.js
@@ -1,21 +1,35 @@
 btn = document.querySelector('.c-menu')
 hg = document.querySelector('.p-menu')
-li = document.querySelector(".l-hgMenu__js")
-
+by = document.querySelector('body')
 
 btn.addEventListener("click", function () {
     if (btn.classList.contains("is-active")) {
         btn.classList.toggle("is-active")
         hg.classList.toggle("is-active")
+        by.classList.toggle("is-active")
     } else {
         btn.classList.toggle("is-active")
         hg.classList.toggle("is-active")
-        gsap.fromTo(".l-hgMenu__js", { x: 200, autoAlpha: 0 }, {
-            x: 0,
-            autoAlpha: 1,
-            duration: 1,
-            stagger: 0.15,
-            repeatDelay: 1,
-        });
+        by.classList.toggle("is-active")
     }
 })
+
+
+var resize = function () {
+
+    var timeoutID = 0;
+    var delay = 500;
+
+    window.addEventListener("resize", function () {
+        clearTimeout(timeoutID);
+        timeoutID = setTimeout(function () {
+
+            btn.classList.remove("is-active");
+            hg.classList.remove("is-active");
+            by.classList.remove("is-active");
+
+        }, delay);
+    }, false);
+};
+
+resize();
